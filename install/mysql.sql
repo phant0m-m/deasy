e9a1ea179685422e0c47add656236134e5b02a94
root <root@mac-ubuntu.(none)>
2014-01-05 19:10:55 +0200

    mysql initial fixed

--- install/mysql.sql
+++ install/mysql.sql
@@ -3,7 +3,7 @@ CREATE TABLE `users` (
 	`username` VARCHAR(128) NOT NULL,
 	`password` VARCHAR(32) NOT NULL,
 	`user_type` VARCHAR(5) NOT NULL,
-	`cca` boolean NOT NULL DEFAULT false;
+	`cca` boolean NOT NULL DEFAULT false,
 	PRIMARY KEY (`id`),
 	UNIQUE KEY (`username`)
 ) ENGINE = InnoDB;
@@ -22,7 +22,7 @@ CREATE TABLE `vhosts` (
 CREATE TABLE `custom_vhosts_configs` (
   `id` INT(11) UNSIGNED AUTO_INCREMENT NOT NULL,
   `vhost_id` INT(11) UNSIGNED NOT NULL,
-  `config` TEXT DEFAULT ''
-  PRIMARY KEY (`vhost_id`),
+  `config` TEXT DEFAULT '',
+  PRIMARY KEY (`id`),
   CONSTRAINT `vhost_config` FOREIGN KEY (`vhost_id`) REFERENCES vhosts(`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB;
